a8bf64c08992ac1c5970c9063f16e8a40ba10a60
Palanivelu.S <cspalni@gmail.com>
2018-04-19 11:17:36 +0530

    added hexaware speakers agenda

--- data/agenda.js
+++ data/agenda.js
@@ -8,7 +8,7 @@ var day1AgendaData = {
         { title: "Go Mobile with Xamarin and Azure", speaker: "Sarthak Mahapatra ", imageURL: "assets/images/speakers/sarthakmahapatra.jpg", desc: "Developing cross-platform mobile apps using Xamarin and Azure.", time: "11.40 AM - 12.30 PM" },
         { title: "Lunch Break", speaker: "", imageURL: "assets/images/coffee.png", desc: "", time: "12.30 PM - 01.30 PM" },
         { title: "You only code once, the Chat Bot Using LUIS", speaker: "Krishnaanand Sivaraj", imageURL: "assets/images/speakers/krishnanandsivaraj.jpg", desc: "Bot is the new UI for web applications. With azure and bot framework in place you build bots with Machine Learning capabilities easily.", time: "01.30 PM - 02.20 PM" },
-        { title: "Building scalable distributed apps  using Azure Event Grid", speaker: "Sriram ", imageURL: "assets/images/speakers/sriram.jpeg", desc: "", time: "02.30 PM - 03.20 PM" },
+        { title: "Learn Machine Learning using Azure ML Studio for Developers", speaker: "Karthikeyan ", imageURL: "assets/images/speakers/karthik.jpg", desc: "", time: "02.30 PM - 03.20 PM" },
         { title: "Break", speaker: "", imageURL: "assets/images/coffee.png", desc: "", time: "03.20 PM - 03.30 PM" },
         { title: "Handson on Azure ML for Developers", speaker: "Karthikeyan", imageURL: "assets/images/speakers/karthik.jpg", desc: "Session to learn about machine learning and how to find the algorithms for a particular problem in machine learning. And use  Azure ML to implement the machine learning algorithm.", time: "03.30 PM - 05.30 PM" },
         { title: "Networking/Goodies & Certificate distribution", speaker: "", imageURL: "assets/images/speakers/certificate.png", desc: "", time: "05.30 PM - 06.00 PM" }
@@ -17,15 +17,15 @@ var day1AgendaData = {
         { title: "Hexaware's Keynote", speaker: "Srinivasan Panchapakesan", imageURL: "assets/images/speakers/srini.jpg", desc: "", time: "09.30 AM - 09.55 AM" },
         { title: "Microsoft RD's KeyNote", speaker: "Venkat Rangan", imageURL: "assets/images/speakers/venkat.jpg", desc: "Digital Transformation with Microsoft Azure", time: "10.00 AM - 10.25 AM" },
         { title: "Overview of the Programme & Break", speaker: "", imageURL: "assets/images/overview.png", desc: "", time: "10.25 AM - 10.40 AM" },
-        { title: "Build Application using Raspberry Pi and Windows 10 IoT Core", speaker: "Murugadoss", imageURL: "assets/images/speakers/murugadoss.jpg", desc: "", time: "10.40 AM - 11.30 AM" },
-        { title: "Build Application using Raspberry Pi and Windows 10 IoT Core", speaker: "Saravanan", imageURL: "assets/images/speakers/sarvn.jpg", desc: "", time: "10.40 AM - 11.30 AM" },
-        { title: "Get Started with Big Data  101", speaker: "Dinesh Kumar Prabakaran ", imageURL: "assets/images/speakers/dineshkumar.png", desc: "Hands-on to deploy a cluster in your own laptop, Hands-on to work with Big Data projects - Hadoop, HBase, Hive, Spark.", time: "11.40 AM - 12.30 PM" },
+        { title: "Building scalable distributed apps  using Azure Event Grid", speaker: "Sriram ", imageURL: "assets/images/speakers/sriram.jpeg", desc: "", time: "10.40 AM - 11.30 AM" },
+        { title: "Nuts & Bolts of Load Balancing in Azure", speaker: "Rakesh Natarajan", imageURL: "assets/images/speakers/rakesh.jpeg", desc: "", time: "11.40 AM - 12.30 PM" },
         { title: "Lunch Break", speaker: "", imageURL: "assets/images/coffee.png", desc: "", time: "12.30 PM - 01.30 PM" },
-        { title: "Hexaware Slot", speaker: "TBD", imageURL: "assets/images/speakers/speaker.png", desc: "", time: "01.30 PM - 02.20 PM" },
-        { title: "Hexaware Slot", speaker: "TBD", imageURL: "assets/images/speakers/speaker.png", desc: "", time: "02.30 PM - 03.20 PM" },
+        { title: "Going Serverless on Azure Functions", speaker: "Kailash Oza", imageURL: "assets/images/speakers/speaker.png", desc: "", time: "01.30 PM - 02.20 PM" },
+        { title: "Build Application using Raspberry Pi with Windows 10 IoT Core and Azure IOT HUB", speaker: "Murugadoss", imageURL: "assets/images/speakers/murugadoss.jpg", desc: "", time: "02.30 PM - 03.20 PM" },
+        { title: "Build Application using Raspberry Pi with Windows 10 IoT Core and Azure IOT HUB", speaker: "Saravanan", imageURL: "assets/images/speakers/sarvn.jpg", desc: "", time: "02.30 PM - 03.20 PM" },
         { title: "Break", speaker: "", imageURL: "assets/images/coffee.png", desc: "", time: "03.20 PM - 03.30 PM" },
-        { title: "Hacking with the Raspberry Pi and Windows 10 IoT Core ", speaker: "Murugadoss", imageURL: "assets/images/speakers/murugadoss.jpg", desc: "", time: "03.30 PM - 05.30 PM" },
-        { title: "Hacking with the Raspberry Pi and Windows 10 IoT Core ", speaker: "Saravanan", imageURL: "assets/images/speakers/sarvn.jpg", desc: "", time: "03.30 PM - 05.30 PM" },
+        { title: "Hacking with the Raspberry Pi with Windows 10 IoT Core and Azure IOT HUB", speaker: "Murugadoss", imageURL: "assets/images/speakers/murugadoss.jpg", desc: "", time: "03.30 PM - 05.30 PM" },
+        { title: "Hacking with the Raspberry Pi with Windows 10 IoT Core and Azure IOT HUB", speaker: "Saravanan", imageURL: "assets/images/speakers/sarvn.jpg", desc: "", time: "03.30 PM - 05.30 PM" },
         { title: "Networking/Goodies & Certificate distribution", speaker: "", imageURL: "assets/images/speakers/certificate.png", desc: "", time: "05.30 PM - 06.00 PM" }
     ],
     openSource: [
@@ -38,7 +38,7 @@ var day1AgendaData = {
         { title: "Lunch Break", speaker: "", imageURL: "assets/images/coffee.png", desc: "", time: "12.30 PM - 01.30 PM" },
         { title: "Continuous Delivery with Kubernetes - 101", speaker: "Sukanya", imageURL: "assets/images/speakers/sukanya.jpg", desc: "Taking the application from development to cloud is neither costly nor tedious with these powerful open source tools like Docker, Kubernetes and Helm. Want to know how? Come and catch up with us.", time: "01.30 PM - 02.20 PM" },
         { title: "Continuous Delivery with Kubernetes - 101", speaker: "Archana", imageURL: "assets/images/speakers/archana.jpeg", desc: "Taking the application from development to cloud is neither costly nor tedious with these powerful open source tools like Docker, Kubernetes and Helm. Want to know how? Come and catch up with us.", time: "01.30 PM - 02.20 PM" },
-        { title: "Devops in Azure using VSTS", speaker: "Saumya Venkataraman", imageURL: "assets/images/speakers/saumya.png", desc: "Build, test and deploy your apps to Azure faster with higher quality using Visual Studio Team Services.", time: "02.30 PM - 03.20 PM" },
+       	{ title: "Get Started with Big Data  101", speaker: "Dinesh Kumar Prabakaran", imageURL: "assets/images/speakers/dineshkumar.png", desc: "Hands-on to deploy a cluster in your own laptop, Hands-on to work with Big Data projects - Hadoop, HBase, Hive, Spark.", time: "02.30 PM - 03.20 PM" },
         { title: "Break", speaker: "", imageURL: "assets/images/coffee.png", desc: "", time: "03.20 PM - 03.30 PM" },
         { title: "Handson with Big Data", speaker: "Dinesh Kumar Prabakaran", imageURL: "assets/images/speakers/dineshkumar.png", desc: "Hands-on to deploy a cluster in your own laptop, Hands-on to work with Big Data projects - Hadoop, HBase, Hive, Spark.", time: "03.30 PM - 05.30 PM" },
         { title: "Networking/Goodies & Certificate distribution", speaker: "", imageURL: "assets/images/speakers/certificate.png", desc: "", time: "05.30 PM - 06.00 PM" }
@@ -47,17 +47,25 @@ var day1AgendaData = {
         { title: "Hexaware's Keynote", speaker: "Srinivasan Panchapakesan", imageURL: "assets/images/speakers/srini.jpg", desc: "", time: "09.30 AM - 09.55 AM" },
         { title: "Microsoft RD's KeyNote", speaker: "Venkat Rangan", imageURL: "assets/images/speakers/venkat.jpg", desc: "Digital Transformation with Microsoft Azure", time: "10.00 AM - 10.25 AM" },
         { title: "Overview of the Programme & Break", speaker: "", imageURL: "assets/images/overview.png", desc: "", time: "10.25 AM - 10.40 AM" },
-        { title: "Architecting Secure Cloud Infrastructure in Azure", speaker: "Akanksha Sharma", imageURL: "assets/images/speakers/akankshasharma.jpg", desc: "A secure infrastructure should be the prime focus of every organization's cloud strategy. The presentation will touch upon ways to protect, detect and control the cloud infrastructure at network, data and management plane. A reference architecture will be presented which will focus upon all these areas by using Azure features as well as 3rd party vendors.", time: "10.40 AM - 11.30 AM" },
-        { title: "Deep Dive into Microsoft Flow", speaker: "Sathish Nadarajan", imageURL: "assets/images/speakers/sathishnadarajan.png", desc: "Deep dive into Microsoft Flow - The session covers the audience of Level 100 to 200 starting from the basics of flow to the introduction about the complex real world problem and implementation of flow.  The major topics covered are, Anatomy of Flow, how to manage the flow, how to debug and importantly comparing the flow with the SharePoint Designer workflow.", time: "11.40 AM - 12.30 PM" },
+        { title: "Case study on the Indexing solution on Azure for Hexaware BPS", speaker: "Saurbah", imageURL: "assets/images/speakers/speaker.png", desc: "", time: "10.40 AM - 11.30 AM" },
+        { title: "Microsoft Azure case studies showcasing our Cloud Migrate & Operate capabilities specific to SAP workloads, Duck Creek applications.", speaker: "Dinesh Melangi", imageURL: "assets/images/speakers/speaker.png", desc: "", time: "11.40 AM - 12.30 PM" },
         { title: "Lunch Break", speaker: "", imageURL: "assets/images/coffee.png", desc: "", time: "12.30 PM - 01.30 PM" },
-        { title: "Nuts & Bolts of Load Balancing in Azure", speaker: "Rakesh Natarajan", imageURL: "assets/images/speakers/rakesh.jpeg", desc: "", time: "01.30 PM - 02.20 PM" },
-        { title: "Build Office 365 hybrid cloud solutions using Azure", speaker: "Ravikumar Sathyamurthy", imageURL: "assets/images/speakers/shakthiravi.jpg", desc: "learn how Microsoft 365 can Secure your Organization and empower your Employees to be more Agile, Productive and Secure.", time: "02.30 PM - 03.20 PM" },
-        { title: "Break", speaker: "", imageURL: "assets/images/coffee.png", desc: "", time: "03.20 PM - 03.30 PM" },
-        { title: "Handson Devops in Azure using VSTS", speaker: "Saumya Venkataraman", imageURL: "assets/images/speakers/saumya.png", desc: "Build, test and deploy your apps to Azure faster with higher quality using Visual Studio Team Services.", time: "03.30 PM - 05.30 PM" },
+        { title: "Microservices using Service Fabric", speaker: "Madan Sudarman", imageURL: "assets/images/speakers/speaker.png", desc: "", time: "01.30 PM - 02.20 PM" },
         { title: "Networking/Goodies & Certificate distribution", speaker: "", imageURL: "assets/images/speakers/certificate.png", desc: "", time: "05.30 PM - 06.00 PM" }
     ],
     newTrack: [
-        { title: "Hexaware's Keynote", speaker: "Karthikeyan", imageURL: "assets/images/speakers/speaker.png", desc: "", time: "09.30 AM - 09.55 AM" },
+        { title: "Hexaware's Keynote", speaker: "Srinivasan Panchapakesan", imageURL: "assets/images/speakers/srini.jpg", desc: "", time: "09.30 AM - 09.55 AM" },
+        { title: "Microsoft RD's KeyNote", speaker: "Venkat Rangan", imageURL: "assets/images/speakers/venkat.jpg", desc: "Digital Transformation with Microsoft Azure", time: "10.00 AM - 10.25 AM" },
+        { title: "Overview of the Programme & Break", speaker: "", imageURL: "assets/images/overview.png", desc: "", time: "10.25 AM - 10.40 AM" },
+        { title: "Architecting Secure Cloud Infrastructure in Azure", speaker: "Akanksha Sharma", imageURL: "assets/images/speakers/akankshasharma.jpg", desc: "A secure infrastructure should be the prime focus of every organization's cloud strategy. The presentation will touch upon ways to protect, detect and control the cloud infrastructure at network, data and management plane. A reference architecture will be presented which will focus upon all these areas by using Azure features as well as 3rd party vendors.", time: "10.40 AM - 11.30 AM" },
+        { title: "Deep Dive into Microsoft Flow", speaker: "Sathish Nadarajan", imageURL: "assets/images/speakers/sathishnadarajan.png", desc: "Deep dive into Microsoft Flow - The session covers the audience of Level 100 to 200 starting from the basics of flow to the introduction about the complex real world problem and implementation of flow.  The major topics covered are, Anatomy of Flow, how to manage the flow, how to debug and importantly comparing the flow with the SharePoint Designer workflow.", time: "11.40 AM - 12.30 PM" },
+        { title: "Lunch Break", speaker: "", imageURL: "assets/images/coffee.png", desc: "", time: "12.30 PM - 01.30 PM" },
+        { title: "Securing your Organization with Microsoft 365", speaker: "Ravikumar Sathyamurthy", imageURL: "assets/images/speakers/shakthiravi.jpg", desc: "learn how Microsoft 365 can Secure your Organization and empower your Employees to be more Agile, Productive and Secure.", time: "01.30 PM - 02.20 PM" },
+        { title: "Devops in Azure using VSTS", speaker: "Saumya Venkataraman", imageURL: "assets/images/speakers/saumya.png", desc: "Build, test and deploy your apps to Azure faster with higher quality using Visual Studio Team Services.", time: "02.30 PM - 03.20 PM" },
+        { title: "Break", speaker: "", imageURL: "assets/images/coffee.png", desc: "", time: "03.20 PM - 03.30 PM" },
+        { title: "Handson Devops in Azure using VSTS", speaker: "Saumya Venkataraman", imageURL: "assets/images/speakers/saumya.png", desc: "Build, test and deploy your apps to Azure faster with higher quality using Visual Studio Team Services.", time: "03.30 PM - 05.30 PM" },
+        { title: "Networking/Goodies & Certificate distribution", speaker: "", imageURL: "assets/images/speakers/certificate.png", desc: "", time: "05.30 PM - 06.00 PM" }
+
     ]
 
 
